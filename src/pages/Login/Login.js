00bba32b4e155a6f06963a9f8b4b8573f9509126
Yusin Kim <yusinkim@Yusinui-MacBookPro.local>
2021-11-04 00:58:48 +0900

    로그인 페이지 및 회원가입 페이지 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,85 @@
 import React from 'react';
 import './Login.scss';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      idInput: '',
+      pwInput: '',
+    };
+  }
+
+  goToMain = () => {
+    this.props.history.push('/Main');
+  };
+
+  updateIdInput = e => {
+    this.setState({
+      idInput: `${e.target.value}`,
+    });
+  };
+
+  updatePwInput = e => {
+    this.setState({
+      pwInput: `${e.target.value}`,
+    });
+  };
+  goToSignUp = () => {
+    this.props.history.push('/Signup');
+  };
+
   render() {
-    return <div />;
+    const { idInput, pwInput } = this.state;
+    const isActiveButton = idInput.includes('@') && pwInput.length >= 5;
+    return (
+      <body className="login">
+        <main>
+          <div className="loginWrap">
+            <div className="bannerSlide">
+              <img src="images/IMG_1227.jpeg" alt="hanoi" />
+            </div>
+            <div className="loginInner">
+              <h1>morning & brunch</h1>
+              <form className="loginForm">
+                <input
+                  id="id"
+                  input
+                  type="text"
+                  placeholder="이메일"
+                  onChange={this.updateIdInput}
+                />
+                <input
+                  id="password"
+                  input
+                  type="password"
+                  placeholder="비밀번호"
+                  onChange={this.updatePwInput}
+                />
+                <button
+                  className={
+                    isActiveButton ? 'changeBtnColor' : 'normalBtnColor'
+                  }
+                  disabled={!isActiveButton}
+                  onClick={this.goToMain}
+                >
+                  로그인
+                </button>
+              </form>
+              <div className="underLoginForm">
+                <div>
+                  <Link to="/Signup">회원가입</Link>
+                </div>
+                <a href="google.com" className="lost">
+                  비밀번호 찾기
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </body>
+    );
   }
 }
 
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,9 +2,174 @@ import React from 'react';
 import './Signup.scss';
 
 class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nameInput: '',
+      idInput: '',
+      pwInput: '',
+      pwcfInput: '',
+      authorInput: '',
+      jobInput: '',
+      introInput: '',
+      emailInput: false,
+    };
+  }
+
+  goToLogin = () => {
+    this.props.history.push('/Login');
+  };
+
+  updateNameInput = e => {
+    this.setState({
+      nameInput: `${e.target.value}`,
+    });
+  };
+
+  updateIdInput = e => {
+    this.setState({
+      idInput: `${e.target.value}`,
+    });
+  };
+
+  updatePwInput = e => {
+    this.setState({
+      pwInput: `${e.target.value}`,
+    });
+  };
+
+  updatePwcfInput = e => {
+    this.setState({
+      pwcfInput: `${e.target.value}`,
+    });
+  };
+
+  updateAuthorInput = e => {
+    this.setState({
+      authorInput: `${e.target.value}`,
+    });
+  };
+
+  updateJobInput = e => {
+    this.setState({
+      jobInput: `${e.target.value}`,
+    });
+  };
+
+  updateIntroInput = e => {
+    this.setState({
+      introInput: `${e.target.value}`,
+    });
+  };
+
+  checkEmail = e => {
+    const regExp =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    this.setState({
+      emailInput: regExp.test(e.target.value),
+    });
+    // console.log('이메일 유효성 검사 :: ', regExp.test(e.target.value));
+  };
+
   render() {
-    return <div />;
+    const {
+      nameInput,
+      idInput,
+      pwInput,
+      pwcfInput,
+      authorInput,
+      jobInput,
+      introInput,
+      emailInput,
+    } = this.state;
+    console.log(emailInput);
+
+    const isActiveButton =
+      nameInput.length >= 2 &&
+      authorInput.length >= 2 &&
+      jobInput.length >= 2 &&
+      idInput.includes('@') &&
+      idInput.includes('.') &&
+      pwInput.length >= 8 &&
+      pwInput === pwcfInput;
+    return (
+      <div className="signUp">
+        <main>
+          <div className="signUpWrap">
+            <div className="signUpLogo">
+              <h1>morning & brunch</h1>
+            </div>
+            <div className="signUpInner">
+              <form className="signUpForm">
+                <div className="inputDiv">
+                  <input
+                    id="id"
+                    type="text"
+                    placeholder="이메일"
+                    onChange={this.updateIdInput}
+                    onChange={this.checkEmail}
+                  />
+                  {emailInput && (
+                    <img
+                      className="check"
+                      src="images/SignUpcheck.png"
+                      alt="check"
+                    />
+                  )}
+                </div>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호 (8자 이상의 영문 대소문자, 숫자, 특수문자 포함)"
+                  onChange={this.updatePwInput}
+                />
+                <input
+                  id="passwordConfirm"
+                  type="password"
+                  placeholder="비밀번호 확인"
+                  onChange={this.updatePwcfInput}
+                />
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="이름"
+                  onChange={this.updateNameInput}
+                />
+                <input
+                  id="authorName"
+                  type="text"
+                  placeholder="작가명(필명)"
+                  onChange={this.updateAuthorInput}
+                />
+                <input
+                  id="authorJob"
+                  type="text"
+                  placeholder="직업"
+                  onChange={this.updateJobInput}
+                />
+                <textarea
+                  id="authorIntro"
+                  maxLength="70"
+                  placeholder="작가소개"
+                  onChange={this.updateIntroInput}
+                />
+                <button
+                  className={
+                    isActiveButton ? 'changeBtnColor' : 'normalBtnColor'
+                  }
+                  disabled={!isActiveButton}
+                  onClick={this.goToLogin}
+                >
+                  회원가입
+                </button>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
 export default Signup;
+// 유효성 검사가 true가 되면 setState로
