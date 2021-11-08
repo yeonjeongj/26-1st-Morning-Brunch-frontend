d74ec1a1701982234220d530e4c30804b98d163f
Yusin Kim <yusinkim@Yusinui-MacBookPro.local>
2021-11-08 11:22:20 +0900

    Mypage 구현중

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -3,7 +3,30 @@ import './Mypage.scss';
 
 class Mypage extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div className="myPage">
+        <main>
+          <div className="myPageCover" />
+          <div className="myPageWrap">
+            <div className="myImg" />
+            <div className="myProfile">
+              <div className="myUser">유신</div>
+              <div className="myUserIntro">유신의 브런치입니다.</div>
+              <dl class="follower">
+                <dd>
+                  <em>구독자</em>
+                  <span>0</span>
+                </dd>
+                <dd>
+                  <em>관심작가</em>
+                  <span>1</span>
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
